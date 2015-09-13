5bd90e8cdbded1ae05bd839d1a5a799622b67a5a
echojiang <jiangao_cool@163.com>
2015-09-13 14:42:23 +0800

    cocos2d-x 常用控件
    20150913

---
+++ code/L08CustomClass/Classes/Ball.cpp
@@ -0,0 +1,16 @@
+//
+//  Ball.cpp
+//  L08CustomClass
+//
+//  Created by jiang on 15-9-11.
+//
+//
+
+#include "Ball.h"
+
+bool Ball::init() {
+    
+    initWithFile("ball.jpg");
+    
+    return true;
+}
